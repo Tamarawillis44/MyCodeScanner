30ca2a37f2eaffb62940831adf755fae0f63c113
Tamarawillis44 <techietl444@gmail.com>
2022-06-28 23:45:59 -0400

    Initial Commit

---
+++ MyCodeScanner/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MyCodeScanner
+//
+//  Created by Tamara laraque on 6/28/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MyCodeScanner/MyCodeScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyCodeScannerApp.swift
+//  MyCodeScanner
+//
+//  Created by Tamara laraque on 6/28/22.
+//
+
+import SwiftUI
+
+@main
+struct MyCodeScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
